ee5f5544795b62fb58b15de884212cd2f7586a09
Sumanmddheshiya <129781628+Sumanmddheshiya@users.noreply.github.com>
2023-08-12 12:34:30 +0530

    Create nfts.js

---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'bili',
+        image: 'QmQYukqH5TYkoQBQVMx5zj628cN1SA18zsWwP1f9HmrPH9'
+    },
+    {
+        name: '1',
+        description: 'Transformer',
+        image: 'QmUQmZBRRiRrQq6hb4YLB1khg722qm3gP9SHrSXFP5jCNXuJb'
+    },
+    {
+        name: '2',
+        description: 'Jadui',
+        image: 'QmV9upz8kQCY1XLoggKpG9uueo2cEiPLfGjL1LZFNqyMqA'
+    },
+    {
+        name: '3',
+        description: 'Shivi',
+        image: 'QmWHJAeP9Cs6KWNRNRuqDDYxg4VPSJThD43thYBjursJQY'
+    },
+    {
+        name: '4',
+        description: 'Girl',
+        image: 'Qmf3q49uziQzucwcA7WMjUdmWUVSiLkLzxuqiaUCng34UJ'
+    },    
+];
+
+module.exports = nfts;
